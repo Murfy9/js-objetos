804a4f7e54eb7c158e1f9c0f887f2e534fe39460
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-22 15:12:13 -0300

    First commit

---
+++ objeto.js
@@ -0,0 +1,8 @@
+const listaCPFs = [11111111111, 2222222222, 333333333333];
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "11112223333"];
+
+const objetoPessoa = {
+  nome: "jose",
+  idade: 32,
+  cpf: "11112223333",
+};
