d73523130fd9115ffbd849ba216b74903eb40047
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-23 14:00:00 -0300

    Funções

---
+++ aula2/funcoes.js
@@ -0,0 +1,19 @@
+const cliente = {
+  nome: "João",
+  idade: 24,
+  email: "joao@firma.com",
+  telefone: ["1155555550", "1144444440"],
+  saldo: 200,
+  efetuaPagamento: function (valor) {
+    if (valor > this.saldo) {
+      console.log("Saldo insuficiente");
+    } else {
+      this.saldo -= valor;
+      console.log(`Pagamento realizado. Novo saldo: ${this.saldo}`);
+    }
+  },
+};
+
+cliente.efetuaPagamento(25);
+
+// Guardar funções como propriedade de um objeto.
