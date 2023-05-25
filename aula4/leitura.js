f920dcc7dbc6ad0ea45b161087984c2b63c92341
Pedro Xavier <pedroxxavier@hotmail.com>
2023-05-25 14:11:22 -0300

    JSON

---
+++ aula4/leitura.js
@@ -0,0 +1,8 @@
+const dados = require("./cliente.json");
+
+console.log(dados);
+console.log(typeof dados);
+
+// JSON é uma sigla para JavaScript Object Notation, ou "Notação de Objeto JavaScript" em português. Ou seja, é um formato que permite a escrita de objetos do JavaScript.
+
+// O require é uma função built-in do Node.js e é usado para carregar módulos de arquivos externos e pacotes instalados globalmente. Ele também pode ser usado para carregar módulos internos do Node.js, como http e fs.
